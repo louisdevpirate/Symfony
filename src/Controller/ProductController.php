1cb822354a85cd1fd81cef25bc15e9a96f5e4c3b
Louis Dole <louis.dole@live.fr>
2023-02-27 10:41:32 +0100

    Création de la route dans controller + page create.html.twig qui contiendra le formulaire d'ajouts de produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -44,4 +44,10 @@ class ProductController extends AbstractController
             'product' => $product,
         ]);
     }
+
+    #[Route('/admin/product/create', name:'product_create')]
+    public function create()
+    {
+        return $this->render('product/create.html.twig');
+    }
 }

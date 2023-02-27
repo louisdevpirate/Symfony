338f91dbea0e51dbd5d26d00239b5629dfb11301
Louis Dole <louis.dole@live.fr>
2023-02-27 18:20:46 +0100

    Exercice 2 + màj documentation

--- documents/Formulaires/documentation.md
+++ documents/Formulaires/documentation.md
@@ -19,3 +19,15 @@ Créez une classe CategoryController avec deux méthodes.
 Exigences :
 Une méthode routée sur /admin/category/create : elle doit juste afficher un fichier Twig avec un titre h1 avec le texte "Créer une catégorie"
 Une méthode routée sur /admin/category/{id}/edit : elle doit afficher un fichier Twig avec un titre h1 contenant le nom de la catégorie correspondant à l'id envoyé dans l'URL
+
+# Exercice 2
+
+Créez le formulaire de création d'une catégorie
+
+Créez une classe de formulaire (make:form) qui s'appellera CategoryType qui ne contiendra qu'un seul champ "name"
+
+Exigences :
+
+La route /admin/category/create doit afficher le formulaire
+On doit aussi gérer la soumission du formulaire avec enregistrement de la nouvelle catégorie dans la base de données !
+On doit enfin rediriger le visiteur vers la page d'accueil
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,16 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
 {
     #[Route('/admin/category/create', name: 'category_create')]
-    public function create(){
-        
-        return $this->render('category/create.html.twig');
+    public function create(FormFactoryInterface $factory, Request $request, CategoryRepository $categoryRepository, EntityManagerInterface $em, SluggerInterface $slugger){
+
+        $category = new Category;
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $category = $form->getData();
+            $category->setSlug(strtolower($slugger->slug($category->getName())));
+
+            $em->persist($category);
+
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('category/create.html.twig', [
+            'formView' => $formView,
+        ]);
     }
 
     #[Route('/admin/category/{id}/edit', name:'category_edit')]

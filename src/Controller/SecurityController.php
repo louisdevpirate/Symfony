45e2a5198ca04c7a90233e62c1d15ca315b380ca
Louis Dole <louis.dole@live.fr>
2023-03-02 12:31:26 +0100

    Création d'un formulaire de connexion dans le controller et affichage dans la page twig

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,10 @@ class SecurityController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function login(): Response
     {
-        return $this->render('security/login.html.twig', []);
+        $form = $this->createForm(LoginType::class);
+
+        return $this->render('security/login.html.twig', [
+            'formView' => $form->createView()
+        ]);
     }
 }

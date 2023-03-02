b9ba2ca0ebb28e1cad1eb1929eecf63c3662a064
Louis Dole <louis.dole@live.fr>
2023-03-02 11:47:53 +0100

    Création de Fixtures dans la BDD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Product;
 use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
@@ -20,34 +21,52 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        
         $faker = Factory::create('fr_FR');
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
-        for($c = 0; $c < 3; $c++){
+        $admin = new User();
+
+        $admin->setEmail("admin@gmail.com")
+            ->setFullName("Admin")
+            ->setPassword("password")
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+
+            $user = new User();
+            $user->setEmail("user$u@gmail.com")
+                ->setFullName($faker->name())
+                ->setPassword("password");
+
+            $manager->persist($user);
+        }
+
+        for ($c = 0; $c < 3; $c++) {
             $category = new Category;
             $category->setName($faker->department)
                 ->setSlug(strtolower($this->slugger->slug($category->getName())));
-            
+
             $manager->persist($category);
-            
-            for($p = 0; $p < mt_rand(15, 20); $p ++){
 
-            $product = new Product;
-            $product->setName($faker->productName)
-                ->setPrice($faker->price(4000, 20000))
-                ->setSlug(strtolower($this->slugger->slug($product->getName())))
-                ->setCategory($category)
-                ->setShortDescription($faker->paragraph())
-                ->setMainPicture($faker->imageUrl(400, 400, true));
+            for ($p = 0; $p < mt_rand(15, 20); $p++) {
 
-            $manager->persist($product);
+                $product = new Product;
+                $product->setName($faker->productName)
+                    ->setPrice($faker->price(4000, 20000))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph())
+                    ->setMainPicture($faker->imageUrl(400, 400, true));
+
+                $manager->persist($product);
             }
         }
 
-        
+
 
         $manager->flush();
     }

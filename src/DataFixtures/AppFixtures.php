857b4ee65485b153e7ae9941ac5e64e9bfb3d180
Louis Dole <louis.dole@live.fr>
2023-03-28 15:12:15 +0200

    Création d'une entité Purchase + Fixtures dans la BDD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,7 @@ use Faker\Factory;
 use App\Entity\User;
 use App\Entity\Product;
 use App\Entity\Category;
+use App\Entity\Purchase;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -40,6 +41,8 @@ class AppFixtures extends Fixture
 
         $manager->persist($admin);
 
+        $users = [];
+
         for ($u = 0; $u < 5; $u++) {
 
             $user = new User();
@@ -49,6 +52,8 @@ class AppFixtures extends Fixture
             $user->setEmail("user$u@gmail.com")
                 ->setFullName($faker->name())
                 ->setPassword($hash);
+
+            $users[] = $user;
 
             $manager->persist($user);
         }
@@ -74,7 +79,22 @@ class AppFixtures extends Fixture
             }
         }
 
+        for ($p = 0; $p < mt_rand(20, 40); $p++) {
+            $purchase = new Purchase;
 
+            $purchase->setFullName($faker->name)
+                ->setAddress($faker->streetAddress)
+                ->setPostalCode($faker->postcode)
+                ->setCity($faker->city)
+                ->setUser($faker->randomElement($users))
+                ->setTotal(mt_rand(2000, 30000));
+
+            if ($faker->boolean(90)) {
+                $purchase->setStatus(Purchase::STATUS_PAID);
+            }
+
+            $manager->persist($purchase);
+        }
 
         $manager->flush();
     }

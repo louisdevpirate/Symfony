799b624228712963a3a7f156f35fe7bd3e4ef714
Louis Dole <louis.dole@live.fr>
2023-04-04 12:32:40 +0200

    Création d'une intention de payement avec Stripe

--- src/Controller/Purchase/PurchasePaymentController.php
+++ src/Controller/Purchase/PurchasePaymentController.php
@@ -2,14 +2,32 @@
 
 namespace App\Controller\Purchase;
 
+use App\Repository\PurchaseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PurchasePaymentController extends AbstractController
 {
     #[Route('/purchase/pay/{id}', name: 'purchase_payment_form')]
-    public function showCardForm()
+    public function showCardForm($id, PurchaseRepository $purchaseRepository)
     {
-        return $this->render('purchase/payment.html.twig');
+        $purchase = $purchaseRepository->find($id);
+
+        if (!$purchase) {
+            return $this->redirectToRoute('cart_show');
+        }
+
+        \Stripe\Stripe::setApiKey('sk_test_51Mt5qnAOvcRLHquyk9lVlkI9CcCrjtTs4Mc3gW36ccah2CkeVrs92X66FwwlY9DQrvTkBC6LT3vmz72HOKWIOd7P009DMFIagj');
+
+        $intent = \Stripe\PaymentIntent::create([
+            'amount' => $purchase->getTotal(),
+            'currency' => 'eur'
+        ]);
+
+        dd($intent);
+
+        return $this->render('purchase/payment.html.twig', [
+            'clientSecret' => $intent->client_secret
+        ]);
     }
 }

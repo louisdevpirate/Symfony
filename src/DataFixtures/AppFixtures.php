f90881fbec476f2fa9daace9e3bfce0c4de7d9f5
Louis Dole <louis.dole@live.fr>
2023-04-13 19:12:21 +0200

    Exercice : créer un automatisme avec Doctrine

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -63,8 +63,7 @@ class AppFixtures extends Fixture
 
         for ($c = 0; $c < 3; $c++) {
             $category = new Category;
-            $category->setName($faker->department)
-                ->setSlug(strtolower($this->slugger->slug($category->getName())));
+            $category->setName($faker->department);
 
             $manager->persist($category);
 
---
+++ src/Doctrine/Listener/CategorySlugListener.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Doctrine\Listener;
+
+use App\Entity\Category;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+
+class CategorySlugListener
+{
+    protected $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function prePersist(Category $category, LifecycleEventArgs $event)
+    {
+        $category = $event->getObject();
+
+        if (!$category instanceof Category) {
+            return;
+        }
+
+        if (empty($category->getSlug())) {
+            // SluggerInterface
+            $category->setSlug(strtolower($this->slugger->slug($category->getName())));
+        }
+    }
+}

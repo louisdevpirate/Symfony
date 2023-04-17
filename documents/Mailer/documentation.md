731f546f0ef5a2a3f542465ddab03909901c4bb4
Louis Dole <louis.dole@live.fr>
2023-04-17 12:46:02 +0200

    Mise en place d'un mailer grâce à MailTrap + màj documentation

--- documents/Mailer/documentation.md
+++ documents/Mailer/documentation.md
@@ -1,2 +1,10 @@
 📖 Documentation officielle de Symfony à propos du Mailer : https://symfony.com/doc/current/mailer.html
 ▶ Mailtrap.io : https://mailtrap.io
+
+Pour envoyer des emails, rien de plus simple, il suffit d'utiliser le composant Mailer de Symfony (en utilisant la commande composer require mailer).
+
+Tout d'abord nous avons le MailerInterface, qui va nous permettre de créer un mail et de l'envoyer. Mais ce dernier ne sait pas par lui-même envoyer réellmeent un email, il se repose sur cette partie là sur des **Transports**. Ce sont ces Transports qui vont permettre l'envoi véritable des emails. 
+
+Ces Transports sont des sortes de **canaux de distribution**, est ce que nous allons décider d'utiliser Gmail, MailChimp, Mailgun etc...?
+
+Par défaut, le transport utilisé est **Smtptransport**, c'est celui que nous avons utilisé dans ce cours avec *MailTrap*. 
---
+++ src/EventDispatcher/ProductViewEmailSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\EventDispatcher;
+
+use App\Event\ProductViewEvent;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+
+class ProductViewEmailSubscriber implements EventSubscriberInterface
+{
+    protected $logger;
+    protected $mailer;
+
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
+    {
+        $this->logger = $logger;
+        $this->mailer = $mailer;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'product.view' => 'sendEmail'
+        ];
+    }
+
+    public function sendEmail(ProductViewEvent $productViewEvent)
+    {
+        // $email = new TemplatedEmail();
+        // $email->from(new Address('contact@mail;com', "Infos de la boutique"))
+        //     ->to("admin@gmail.com")
+        //     ->text("Un visiteur est en train de voir la page du produit n°" . $productViewEvent->getProduct()->getId())
+        //     ->htmlTemplate('emails/product_view.html.twig')
+        //     ->context([
+        //         'product' => $productViewEvent->getProduct()
+        //     ])
+        //     ->subject("Visite du produit n°" . $productViewEvent->getProduct()->getId());
+
+        // $this->mailer->send($email);
+
+        $this->logger->info("Email envoyé pour la commande n° " . $productViewEvent->getProduct()->getId());
+    }
+}
